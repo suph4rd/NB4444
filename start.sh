89fdb7697c84f6c5540e713d73504fd06d95ceb6
suph4rd <suph4rd575@gmail.com>
2021-08-31 00:42:13 +0300

    feat: add docker configuration

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/suph4rd/PycharmProjects/B45/NB4444/
+cd /home/suph4rd/PycharmProjects/b4_dev/NB4444/
 source ../venv/bin/activate
 python manage.py runserver 0.0.0.0:9999
 pause

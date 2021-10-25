9bf08e3e36b25dd1e0d30ebcb17b1f690c692bf8
suph4rd <suph4rd575@gmail.com>
2021-10-25 19:16:22 +0300

    feat: add B4URLTests

--- B4/tests.py
+++ B4/tests.py
@@ -1,3 +1,129 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from B4 import models
+
+
+class B4URLTests(TestCase):
+    custom_client = None
+    custom_variables = dict()
+
+    def setUp(self) -> None:
+        client = Client()
+        self.custom_variables['user'] = User.objects.create_superuser(
+            email='root@root.root',
+            username='root',
+            password='12345TestCase'
+        )
+        client.login(username='root', password='12345TestCase')
+        self.custom_client = client
+
+        # plans
+        self.custom_variables['plan'] = models.Plan.objects.create(user=self.custom_variables.get("user"), name="plan")
+        # section
+        self.custom_variables['section'] = models.Section.objects.create(name="section", is_active=True)
+        # tasks
+        self.custom_variables['task'] = models.Task.objects.create(
+            plan=self.custom_variables.get("plan"),
+            section=self.custom_variables.get("section"),
+            description="",
+            is_ready=False
+        )
+
+    def test_url_b4_general(self):
+        url = "b4:general"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_login(self):
+        url = "b4:login"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_logout(self):
+        url = "b4:logout"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 302)
+
+    def test_url_b4_default_deductions(self):
+        url = "b4:default_deductions"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_note(self):
+        url = "b4:note"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_plan_list(self):
+        url = "b4:plan_list"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_plan_detail(self):
+        url = "b4:plan_detail"
+        obj = models.Plan.objects.last()
+        self.assertEqual(bool(obj), True)
+        pk = obj.pk
+        response = self.custom_client.get(reverse(url, kwargs={"pk": pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_plan_create(self):
+        url = "b4:plan_create"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_plan_update(self):
+        url = "b4:plan_update"
+        obj = models.Plan.objects.last()
+        self.assertEqual(bool(obj), True)
+        pk = obj.pk
+        response = self.custom_client.get(reverse(url, kwargs={"pk": pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_plan_delete(self):
+        url = "b4:plan_delete"
+        obj = models.Plan.objects.last()
+        # obj = models.Plan.objects.create(user=self.custom_variables.get('user'), name="for delete")
+        self.assertEqual(bool(obj), True)
+        pk = obj.pk
+        response = self.custom_client.get(reverse(url, kwargs={"pk": pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_plan_today_create(self):
+        url = "b4:plan_today_create"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 302)
+
+    def test_url_b4_plan_autocomplete(self):
+        url = "b4:plan_autocomplete"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_task_create(self):
+        url = "b4:task_create"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_task_update(self):
+        url = "b4:task_update"
+        obj = models.Task.objects.last()
+        self.assertEqual(bool(obj), True)
+        pk = obj.pk
+        response = self.custom_client.get(reverse(url, kwargs={"pk": pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_task_delete(self):
+        url = "b4:task_delete"
+        obj = models.Task.objects.last()
+        # obj = models.Task.objects.create(user=self.custom_variables.get('user'), name="for delete")
+        self.assertEqual(bool(obj), True)
+        pk = obj.pk
+        response = self.custom_client.get(reverse(url, kwargs={"pk": pk}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_b4_bot_response(self):
+        url = "b4:bot_response"
+        response = self.custom_client.get(reverse(url))
+        self.assertEqual(response.status_code, 302)

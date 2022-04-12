b96c7b6c307acd6108dc402464caef7d931ba3d8
suph4rd <suph4rd575@gmail.com>
2022-04-12 23:57:57 +0300

    fix: change api logic

--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-import copy
 import threading
 
 from rest_framework import status
@@ -7,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api import serializers
+from api.mixins import CRWithUserMixin, ListFilterMixin
 from api.permissions import IsSuperUserOrOwnerPermission
 from B4 import models as b4_models
 
@@ -19,50 +19,13 @@ def get_bot_info_view(request):
     return Response(status=status.HTTP_200_OK)
 
 
-class ListFilterModelViewSet(ModelViewSet):
-    permission_classes = [IsSuperUserOrOwnerPermission, ]
-
-    def get_int_value(self, value) -> int:
-        match value:
-            case int(value):
-                return value
-            case str(value) if value.isdigit() :
-                return int(value)
-            case list(value) if len(value) == 1:
-                return self.get_int_value(value[0])
-        raise ValueError({"error": f"{value} is not convent to Integer"})
-
-    def list(self, request, *args, **kwargs):
-        offset = request.GET.get('offset')
-        limit = request.GET.get('limit')
-        if offset or limit:
-            try:
-                int_offset = self.get_int_value(offset)
-                int_limit = self.get_int_value(limit)
-                if int_offset and int_limit and int_offset >= int_limit:
-                    raise ValueError(f"offset: {offset} больше либо равен limit: {limit}")
-                self.queryset = self.queryset[int_offset: int_limit]
-            except Exception as err:
-                print(err)
-                return Response({"error": str(err)}, status=400)
-        return super().list(request, *args, **kwargs)
-
-
-def made_list_filter_model_viewset_class(local_models, local_serializer, local_permission_classes: list = None):
-    class CustomListFilterModelViewSet(ListFilterModelViewSet):
-        queryset = local_models.objects.all()
-        serializer_class = local_serializer
-        if local_permission_classes:
-            permission_classes = local_permission_classes
-    return CustomListFilterModelViewSet
-
-
-class DefaultDeductionListFilterModelViewSet(ListFilterModelViewSet):
+class DefaultDeductionListFilterModelViewSet(ListFilterMixin, ModelViewSet):
     queryset = b4_models.DefaultDeductions.objects.all()
     serializer_class = serializers.get_model_serializer_class(
         b4_models.DefaultDeductions,
-        local_exclude=['created_at', 'updated_at']
+        local_exclude=['created_at', 'updated_at', 'delete_datetime', 'is_delete']
     )
+    permission_classes = [IsSuperUserOrOwnerPermission]
 
     @action(methods=['get'], detail=False)
     def last(self, request, *args, **kwargs):
@@ -81,72 +44,47 @@ class DefaultDeductionListFilterModelViewSet(ListFilterModelViewSet):
         return Response(serializer.data)
 
 
-class CurrentSerializerMixin:
-    model = None
-    depth = 0
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.serializer_class = self.get_current_serializer_class()
-
-    def get_current_serializer_class(self, local_depth=None):
-        return serializers.get_model_serializer_class(
-            self.model,
-            local_exclude=['created_at', 'updated_at'],
-            local_depth=local_depth if isinstance(local_depth, int) else self.depth
-        )
-
-    def list(self, request, *args, **kwargs):
-        if hasattr(self.model, 'user') and request.user.is_authenticated and not request.user.is_superuser:
-            self.queryset = self.queryset.filter(user=request.user)
-        return super().list(request, *args, **kwargs)
-
-    def create(self, request, *args, **kwargs):
-        self.serializer_class = self.get_current_serializer_class(local_depth=0)
-        return super().create(request, *args, **kwargs)
-
-    def update(self, request, *args, **kwargs):
-        self.serializer_class = self.get_current_serializer_class(local_depth=0)
-        return super().update(request, *args, **kwargs)
-
-    def partial_update(self, request, *args, **kwargs):
-        self.serializer_class = self.get_current_serializer_class(local_depth=0)
-        return super().partial_update(request, *args, **kwargs)
-
-
-class PlanModelListFilterModelViewSet(CurrentSerializerMixin, ListFilterModelViewSet):
+class PlanModelListFilterModelViewSet(ListFilterMixin, CRWithUserMixin, ModelViewSet):
     model = b4_models.Plan
     queryset = model.objects.all()
+    serializer_class = serializers.get_model_serializer_class(
+        b4_models.DefaultDeductions,
+        local_exclude=['created_at', 'updated_at', 'delete_datetime', 'is_delete']
+    )
+    permission_classes = [IsSuperUserOrOwnerPermission]
+
+    def retrieve(self, request, *args, **kwargs):
+        self.serializer_class = serializers.ListPlanSerializer
+        return super().retrieve(request, *args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        self.serializer_class = serializers.ListPlanSerializer
+        return super().list(request, *args, **kwargs)
 
 
-class TaskModelListFilterModelViewSet(CurrentSerializerMixin, ListFilterModelViewSet):
+class TaskModelListFilterModelViewSet(ListFilterMixin, CRWithUserMixin, ModelViewSet):
     model = b4_models.Task
     queryset = model.objects.all()
-    depth = 2
+    serializer_class = serializers.get_model_serializer_class(
+        b4_models.DefaultDeductions,
+        local_exclude=['created_at', 'updated_at', 'delete_datetime', 'is_delete']
+    )
+    permission_classes = [IsSuperUserOrOwnerPermission]
 
-    def list(self, request, *args, **kwargs):
-        if request.user.is_authenticated and not request.user.is_superuser:
-            self.queryset = self.queryset.filter(plan__user=request.user)
-        return super().list(request, *args, **kwargs)
 
-
-class NoteModelListFilterModelViewSet(CurrentSerializerMixin, ListFilterModelViewSet):
+class NoteModelListFilterModelViewSet(ListFilterMixin, CRWithUserMixin, ModelViewSet):
     model = b4_models.Note
     queryset = model.objects.all()
+    permission_classes = [IsSuperUserOrOwnerPermission]
+    serializer_class = serializers.get_model_serializer_class(
+        b4_models.Note,
+        local_exclude=['created_at', 'updated_at', 'delete_datetime', 'is_delete']
+    )
+
+    def retrieve(self, request, *args, **kwargs):
+        self.serializer_class = serializers.ListNoteSerializer
+        return super().retrieve(request, *args, **kwargs)
 
     def list(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return Response({"error": "Пользователь не авторизован!"}, status=403)
-        self.queryset = self.queryset.filter(user=request.user)
+        self.serializer_class = serializers.ListNoteSerializer
         return super().list(request, *args, **kwargs)
-
-    def create(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return Response({"error": "Пользователь не авторизован!"}, status=403)
-        data = copy.deepcopy(request.data)
-        data['user'] = request.user.id
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)

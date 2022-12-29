c9bda2d915056dec902c5224c367060d32aea11b
suph4rd <suph4rd575@gmail.com>
2022-12-30 01:04:57 +0400

    fix: refactor tests

--- B4/tests/tests_common.py
+++ B4/tests/tests_common.py
@@ -4,31 +4,8 @@ from django.urls import reverse
 from B4 import models
 
 
-class B4URLTests(TestCase):
+class B4URLCommonMixin:
     custom_client = None
-    custom_variables = {}
-
-    def setUp(self) -> None:
-        client = Client()
-        self.custom_variables['user'] = models.User.objects.create_superuser(
-            email='root@root.root',
-            username='root',
-            password='12345TestCase'
-        )
-        client.login(username='root', password='12345TestCase')
-        self.custom_client = client
-
-        # plans
-        self.custom_variables['plan'] = models.Plan.objects.create(user=self.custom_variables.get("user"), name="plan")
-        # section
-        self.custom_variables['section'] = models.Section.objects.create(name="section", is_active=True)
-        # tasks
-        self.custom_variables['task'] = models.Task.objects.create(
-            plan=self.custom_variables.get("plan"),
-            section=self.custom_variables.get("section"),
-            description="",
-            is_ready=False
-        )
 
     def test_url_b4_general(self):
         url = "b4:general"
@@ -84,7 +61,6 @@ class B4URLTests(TestCase):
     def test_url_b4_plan_delete(self):
         url = "b4:plan_delete"
         obj = models.Plan.objects.last()
-        # obj = models.Plan.objects.create(user=self.custom_variables.get('user'), name="for delete")
         self.assertEqual(bool(obj), True)
         pk = obj.pk
         response = self.custom_client.get(reverse(url, kwargs={"pk": pk}))
@@ -94,11 +70,6 @@ class B4URLTests(TestCase):
         url = "b4:plan_today_create"
         response = self.custom_client.get(reverse(url))
         self.assertEqual(response.status_code, 302)
-
-    # def test_url_b4_plan_autocomplete(self):
-    #     url = "b4:plan_autocomplete"
-    #     response = self.custom_client.get(reverse(url))
-    #     self.assertEqual(response.status_code, 200)
 
     def test_url_b4_task_create(self):
         url = "b4:task_create"
@@ -116,13 +87,60 @@ class B4URLTests(TestCase):
     def test_url_b4_task_delete(self):
         url = "b4:task_delete"
         obj = models.Task.objects.last()
-        # obj = models.Task.objects.create(user=self.custom_variables.get('user'), name="for delete")
         self.assertEqual(bool(obj), True)
         pk = obj.pk
         response = self.custom_client.get(reverse(url, kwargs={"pk": pk}))
         self.assertEqual(response.status_code, 200)
 
-    # def test_url_b4_bot_response(self):
-    #     url = "b4:bot_response"
-    #     response = self.custom_client.get(reverse(url))
-    #     self.assertEqual(response.status_code, 302)
+
+class B4URLTestsSuperuser(B4URLCommonMixin, TestCase):
+    custom_variables: dict = None
+
+    def setUp(self) -> None:
+        client = Client()
+        self.custom_variables['user'] = models.User.objects.create_superuser(
+            email='root@root.root',
+            username='root',
+            password='12345TestCase'
+        )
+        client.login(username='root', password='12345TestCase')
+        self.custom_client = client
+
+        # plans
+        self.custom_variables['plan'] = models.Plan.objects.create(user=self.custom_variables.get("user"), name="plan")
+        # section
+        self.custom_variables['section'] = models.Section.objects.create(name="section", is_active=True)
+        # tasks
+        self.custom_variables['task'] = models.Task.objects.create(
+            plan=self.custom_variables.get("plan"),
+            section=self.custom_variables.get("section"),
+            description="",
+            is_ready=False
+        )
+
+
+class B4URLTestsUser(B4URLCommonMixin, TestCase):
+    custom_variables: dict = None
+
+    def setUp(self) -> None:
+        client = Client()
+        self.custom_variables = {'user': models.User.objects.create_user(
+            email='user@user.user',
+            username='user',
+            password='12345TestCase'
+        )}
+        client.login(username='user', password='12345TestCase')
+        self.custom_client = client
+
+        # plans
+        self.custom_variables['plan'] = models.Plan.objects.create(user=self.custom_variables.get("user"), name="plan_user")
+        # section
+        self.custom_variables['section'] = models.Section.objects.create(name="section_user", is_active=True)
+        # tasks
+        self.custom_variables['task'] = models.Task.objects.create(
+            plan=self.custom_variables.get("plan"),
+            section=self.custom_variables.get("section"),
+            description="",
+            is_ready=False
+        )
+
